3aac1fb49311c158f36275e93631aaf24e0e6c7a
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-07 10:11:21 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accId=122334 //one time assign and declare and block scope
+var accEmail="xyz@gmail.com"//multiple time declare and assign nd block scope and not used much
+let accPassword="xyz"//one time declare multiple access and global scope and widely used
+accCity="jaipur"
+let accState
+
+//const accId=2//
+
+var accEmail="abc@gmail.com"
+ accPassword="abc"
+accCity="pune"
+
+console.log(accId,accCity,accEmail,accState,accPassword)
