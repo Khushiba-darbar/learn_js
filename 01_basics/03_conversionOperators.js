fd96e9dc79f77b7ad52a837d9fd523b51952474a
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-07 18:20:28 +0000

    conversion operators

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,77 @@
+/*let score ="33abc"
+let ans=true
+ 
+console.log(typeof(score))
+console.log(typeof(ans))
+
+let ValueInNumber
+ ValueInNumber=Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+ValueInNumber=Number(ans)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+/*
+"33"=>33 ie string nu number thse
+"33abc"=>nan
+true =1 false=0*/
+//covertsion into boolean//
+/*let IsLoggedIn="khushi"
+
+console.log(typeof(IsLoggedIn))
+/*1=true 0=false ""=false "khushi"=true*/
+/*let booleanIsLoggedIn=Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let someno=33
+
+console.log(someno)
+
+let stringsomeno=String(someno)
+console.log(typeof(stringsomeno))
+console.log(stringsomeno)
+*/
+// ************************operations*********************************** //
+
+let value=9
+let negvalue=-value
+console.log(negvalue)
+
+console.log(5+10)
+console.log(5-10)
+console.log(5*10)
+console.log(5/10)
+console.log(2**5)
+console.log(12%11)
+
+let str1="khushi"
+let str2="darbar"
+let str3=str1+str2
+console.log(str3)
+/*aa type na conversion ma if string pela hase to e pachi  na elements ne bi string in jem treat krse and
+if aagad number hase to aagad na number ne add krse nd thenstring ama join krse bs*/
+/*console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+//not used proffesionally
+
+console.log(3+4*5%3)//dont write like this
+console.log(((3+4)*5)%3)//use parenthesis
+*/
+//not written 
+console.log(+true)
+console.log(+"")
+
+let n1,n2,n3
+n1=n2=n3=2+2
+
+let gamecounter=100
+console.log(gamecounter)
+++gamecounter//return value after incrementing
+console.log(gamecounter)
+gamecounter++//return value before incrementing
+console.log(gamecounter)
