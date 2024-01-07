c6b9fed6338bdfb4751a1f8d2b59644d9e5ed945
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-07 16:15:43 +0530

    02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all jscode as newer version
+//alert (khushi)//we are using js not browser soo pop up notification wont come   
+//TC39 AND MDN IS VERY USEFUL FOR REFERENCE//
+
+let name="khushi"
+let ans=true
+let age=18
+let id=null
+
+/* data types
+1.number its range is 2^53
+2.bigint for higher value int
+3.string ""
+4.boolean true/false
+5.null -its a standalone value/it is empty ie value assigned is 0
+6.undefined-variable declare but not assigned
+7.symbol -unique*/
+
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
+
