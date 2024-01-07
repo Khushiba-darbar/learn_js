a3132cbee293d194673f45218847d4b819d1a060
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-07 18:34:12 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+//for comparision make sure you compare 2 same datatypes//
+console.log("2">1)
+console.log("02">1)
+
+
+//avoid this type of coversion
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)//in this null is automatically converted into number
+console.log(null<0)
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined<0)
+
+//=== strict check means datatype of both
