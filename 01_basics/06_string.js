ebf9e6dce283d6c4baf3d281179fa1961d10e957
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-08 13:58:09 +0000

    container config done

---
+++ 01_basics/06_string.js
@@ -0,0 +1,16 @@
+let name="khushi"
+let repoVal=8
+
+console.log(name+repoVal+"value")//very outdated no one uses it nowadays
+console.log(`hello my name is ${name} and my repository value is ${repoVal}`)
+//modern way of concatenating string byusing whole sentence in`_` and for printing value use$sign then 
+//curly braches and name ir ${name}
+
+console.log(name[0])
+console.log(name.__proto__)
+
+console.log(name.length)
+console.log(name.endsWith());
+console.log(name.bold())
+console.log(name.toUpperCase())
+console.log(name.charAt('u'))
