5a256e1445c36ec35564c61038d25c9958b419bc
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-17 14:29:14 +0000

    container config done

--- 01_basics/07_numAndMath.js
+++ 01_basics/07_numAndMath.js
@@ -17,7 +17,15 @@ const hundreds=100000000
 
 console.log(Math
 )
-console.log(Maths.abs.abs(-4))
-console.log(Math.roundof(4,6))
-console.log(Math.ceil(4.2))
-console.log(math.floor(4.9))
+console.log(Math.abs(-4))//sign ne opp krse
+console.log(Math.round(4.9876546789,6))//rounf\d off krse
+console.log(Math.ceil(4.2))//rrot no
+console.log(Math.floor(4.9))//after no
+console.log(Math.min(3,5,7,8,1))//find min no
+console.log(Math.random())//value obt is between 0 and 1
+console.log((Math.random()*10)+1)//*10 beacuse 1 decimal ma lavu hoi nd +1 ensures tht value is not zero
+console.log(Math.floor((Math.random()*10)+1))//to get floor value
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
