e4fad666af55669039254a418111f7ffdfc62efb
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-01-21 20:26:55 +0000

    more about functions

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,26 @@
+//whnen you dont know how many values wll be passed
+function calccartprice(val1,val2,...num1)//...=rest 
+{
+return  num1
+}console.log(calccartprice(123,500,467,678));
+//val 1 and  val2 will be given the vale and rest will be kept in array ;rest value is always in array
+
+const user={name:"khushi",
+produvct:"pen",
+price:35}
+
+function handleobject(anyobject)
+{console.log(` username is ${anyobject.name} amd product is${anyobject.produvct} and its price is
+ ${anyobject.price}`);}
+
+
+//handleobject()
+//{name:"sam",produvct:"paper",price:10}
+
+const arrayy=[100,200,300,400]
+
+function returnssecvalue(getarray){
+return getarray[2]
+}
+console.log(handleobject(user));
+console.log(returnssecvalue(arrayy));
