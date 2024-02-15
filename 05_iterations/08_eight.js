ccd40425c71cc3a89b2a2d90a41b0f498b81c555
Khushiba-darbar <145755690+Khushiba-darbar@users.noreply.github.com>
2024-02-15 12:28:48 +0000

    container config done

--- 05_iterations/08_eight.js
+++ 05_iterations/08_eight.js
@@ -1,11 +1,30 @@
 const munums=[1,2,3,4,5,6,7]
 //very much used in billing 
-let mytotal=munums.reduce(function(acc,cv){
-    console.log(`acc:${acc} and cv is ${cv}`);
-    return acc+cv},0)
+let mytotal=munums.reduce(function (accumulator,cv){
+    console.log(`acc:${accumulator} and cv is ${cv}`);
+    return accumulator+cv;},8)
 console.log(mytotal);
 
-mytotal=munums.reduce((acc,cv)=>
-    (console.log(`acc:${acc} and cv is ${cv}`)
-    acc+cv),0;
-    console.log(mytotal);
+
+
+    const shoppingCart=[{
+        course:"learn js",
+        price:12999
+    },{
+        course:"python",
+        price:9999
+    },{
+        course:"mob dev",
+        price:19999
+    },{
+        course:"jc language",
+        price:5999
+    },{
+        course:"graphics",
+        price:29999
+    }
+]
+let totalbill=shoppingCart.reduce(
+    (acc,item)=>acc+item.price,0)
+    console.log(totalbill);
+
